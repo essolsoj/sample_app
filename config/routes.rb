d3b84579deeeb203d660c58f645928f3e3f69bb3
jordi <user@user-PC.(none)>
2013-04-15 10:14:42 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
+#map GET static_pages/home to StaticPages controller with action home
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
